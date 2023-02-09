9a228cfa64d3141ae2cb284ddee091d86152ad17
Randall Helms <124309699+datarampage@users.noreply.github.com>
2023-02-09 08:22:19 +0100

    Add files via upload

--- r_code/substack_cleaning_example.R
+++ r_code/substack_cleaning_example.R
@@ -1,8 +1,40 @@
 library(readxl)
 library(tidyverse)
-library(rtools)
 library(lubridate)
 library(googlesheets4)
+
+#function for cleaning column names
+
+tidycols <- function(df) {
+  require(snakecase)
+  
+  dfnames <- colnames(df)
+  
+  dfnames <- gsub('#','nr',dfnames,fixed=TRUE)
+  
+  dfnames <- gsub('%','pct',dfnames,fixed=TRUE)
+  
+  dfnames <- gsub('Ø','avg',dfnames,fixed=TRUE)
+  
+  dfnames <- gsub('$','usd',dfnames,fixed=TRUE)
+  
+  dfnames <- gsub('€','eur',dfnames,fixed=TRUE)
+  
+  dfnames <- gsub('£','gbp',dfnames,fixed=TRUE)
+  
+  dfnames <- to_snake_case(dfnames,sep_out = "_")
+  
+  dfnames <- tolower(gsub(" ","_",dfnames))
+  
+  dfnames <- gsub(".","_",dfnames,fixed=TRUE)
+  
+  dfnames <- gsub("/","_per_",dfnames,fixed=TRUE)
+  
+  colnames(df) <- dfnames
+  
+  return(df)
+  
+}
 
 #read the file in
 
